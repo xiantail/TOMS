af7c9c7976d73b2a02f22be4e0601ad7c4c3ce90
Ken Xu <kenmygw@me.com>
2016-01-20 18:31:45 +0900

    Create Class Simulator

--- time_simulator.py
+++ time_simulator.py
@@ -1,49 +1,128 @@
 from datetime import datetime, timedelta
 from time import sleep
 import math
+from train_status import *
+from train_status import StationZone as SZ
+from train_status import Track as TR
+from train_status import Garage as GR
+from train_status import Lane as LN
 
-def calc_duration(distance, speed=(0.0, 70.0, 0.0), acceleration=3.0, decceleration=3.0, rounding=10, debug=False):
+import csv
+
+class Simulator():
     '''
-    :param distance: distance between stations in Kilometer
-    :param speed: (start speed, max speed, end speed)
-    :param acceleration: km/h/sec
-    :param decceleration: km/h/sec
-    :param rounding: 10,15,30,60
-    :return: duration time in seconds
+    Sample data can be generated with function create_sample_csv in train_status.py
     '''
-    start_speed = speed[0]
-    max_speed = speed[1]
-    end_speed = speed[2]
-    # time to reach max_speed in sec.
-    ttrm = math.ceil((max_speed - start_speed) / acceleration)
-    # distance to reach max_speed in km
-    dtrm = round((max_speed - start_speed) / 2 * ttrm / 3600, 3)
-    # time to reach end_speed in sec.
-    ttre = math.ceil((max_speed - end_speed) / decceleration)
-    # distance to reach end_speed in km
-    dtre = round((max_speed - end_speed) / 2 * ttre / 3600, 3)
-    # distance with max_speed
-    dwms = distance - dtrm - dtre
-    if debug:
-        print('ttrm: %s' % ttrm)
-        print('dtrm: %s' % dtrm)
-        print('ttre: %s' % ttre)
-        print('dtre: %s' % dtre)
-        print('dwms: %s' % dwms)
-    if dwms > 0:
-        # time with max_speed
-        twms = math.ceil(dwms / max_speed * 3600)
+    station_name_dict = {}
+    station_list = []
+    station_dict = {}
+    track_list = []
+    track_dict = {}
+    garage_list = []
+    garage_dict = {}
+    lane_list = []
+    lane_dict = {}
+
+    @classmethod
+    def load_data(cls):
+        # Step.1 load station name
+        station_name_dict = {}
+        with open('station_list.csv', 'rt') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                # List is not required as this only be used to get name text
+                Simulator.station_name_dict[row['code']] = row['name']
+
+        # Step.2 load zone specific station data
+        with open('station_zone.csv', 'rt') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                row['name'] = Simulator.station_name_dict[row['code']]
+                #code, name, zone, center_position, station_range
+                sz = SZ(code=row['code'], name=row['name'], zone=row['zone'],
+                        center_position=row['center_position'], station_range=row['station_range'])
+                Simulator.station_dict[row['code']]=sz
+                Simulator.station_list.append(sz)
+
+        # Step.3 load tracks
+        with open('track_list.csv', 'rt') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                #name, zone, speed, category, track_range
+                tr = TR(name=row['name'], zone=row['zone'], speed=row['speed'], category=row['category'],
+                        track_range=['track_range'])
+                Simulator.track_dict[row['name']]=tr
+                Simulator.track_list.append(tr)
+
+        # Step.4 load garages
+        with open('garage_list.csv', 'rt') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                #code, lanes
+                gr = GR(code=row['code'], lanes=row['lanes'])
+                Simulator.garage_dict[row['code']]=gr
+                Simulator.garage_list.append(gr)
+
+        # Step.5 load lanes
+        with open('lane_list.csv', 'rt') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                connection = []
+                #[stationzone], name, offset, lane_range, connection
+                stationzone = Simulator.station_dict[row['code']]
+                for elem in row['connection']:
+                    if 'G' in elem: # garages
+                        connection.append(Simulator.garage_dict[elem])
+                    else:
+                        connection.append(Simulator.track_dict[elem])
+                ln = LN(stationzone=stationzone, name=row['name'], offset=row['offset'],
+                        lane_range=row['lane_range'], connection=connection)
+                Simulator.station_dict[row['code']].assign_lane(ln)
+                Simulator.lane_list.append(ln)
+
+    @classmethod
+    def calc_duration(cls, distance, speed=(0.0, 70.0, 0.0), acceleration=3.0, decceleration=3.0, rounding=10, debug=False):
+        '''
+        :param distance: distance between stations in Kilometer
+        :param speed: (start speed, max speed, end speed)
+        :param acceleration: km/h/sec
+        :param decceleration: km/h/sec
+        :param rounding: 10,15,30,60
+        :return: duration time in seconds
+        '''
+        start_speed = speed[0]
+        max_speed = speed[1]
+        end_speed = speed[2]
+        # time to reach max_speed in sec.
+        ttrm = math.ceil((max_speed - start_speed) / acceleration)
+        # distance to reach max_speed in km
+        dtrm = round((max_speed - start_speed) / 2 * ttrm / 3600, 3)
+        # time to reach end_speed in sec.
+        ttre = math.ceil((max_speed - end_speed) / decceleration)
+        # distance to reach end_speed in km
+        dtre = round((max_speed - end_speed) / 2 * ttre / 3600, 3)
+        # distance with max_speed
+        dwms = distance - dtrm - dtre
         if debug:
-            print('twms: %s' % twms)
-    else:   #distance is too short to reach max_speed
-        print('Max speed is too high in this section')
-        return 0
-    # rounding
-    if rounding not in (10, 15, 30, 60):
-        rounding = 10
-    duration = ttrm + twms + ttre
-    duration += (rounding - duration % rounding)
-    return duration
+            print('ttrm: %s' % ttrm)
+            print('dtrm: %s' % dtrm)
+            print('ttre: %s' % ttre)
+            print('dtre: %s' % dtre)
+            print('dwms: %s' % dwms)
+        if dwms > 0:
+            # time with max_speed
+            twms = math.ceil(dwms / max_speed * 3600)
+            if debug:
+                print('twms: %s' % twms)
+        else:   #distance is too short to reach max_speed
+            print('Max speed is too high in this section')
+            return 0
+        # rounding
+        if rounding not in (10, 15, 30, 60):
+            rounding = 10
+        duration = ttrm + twms + ttre
+        duration += (rounding - duration % rounding)
+        return duration
 
 if __name__ == '__main__':
     # set basedate
--- train_status.py
+++ train_status.py
@@ -1,5 +1,7 @@
 from time import sleep
 from datetime import datetime, timedelta
+import csv
+import copy
 
 class TrainStatus():
     '''
@@ -46,6 +48,13 @@ class StationZone(Station):
         self.zone = zone
         self.center_position = center_position
         self.station_range = station_range   #tuple(0.000, 0.400)
+        self.assigned_lane = []
+
+    def assign_lane(self, lanes):
+        for lane in lanes:
+            if type(lane)=='':
+                self.assigned_lane.append(lane)
+                print('%s is assigned to station %s' % (lane.name, self.name))
 
 class OccupationHandling():
     def __init__(self):
@@ -70,35 +79,36 @@ class OccupationHandling():
 class Lane(OccupationHandling):
     '''
     Lane master in stations
+    'code', 'zone', 'name', 'priority', 'offset', 'lane_range', 'connection'
     '''
-    def __init__(self, stationzone, lane, offset, restriction, lane_range):
+    def __init__(self, stationzone, name, offset, lane_range, connection):
         super().__init__()
         self.stationzone = [stationzone]  #StationZone list
-        self.lane = lane
+        self.name = name
         self.offset = offset
-        self.restriction = restriction
         self.lane_range = lane_range
-        self.connection = []
+        self.connection = connection
 
 class Track(OccupationHandling):
     '''
     Track master
+    'name', 'zone', 'speed', 'category', 'track_range'
     '''
-    def __init__(self, zone, name, speed, type, restriction):
+    def __init__(self, name, zone, speed, category, track_range):
         super().__init__()
         self.zone = zone
         self.name = name
         self.speed = speed
-        self.type = type    #Double tracks or Single track
-        self.restriction = restriction
+        self.category = category    #Double tracks or Single track
+        self.track_range = track_range
         self.occupied = None
 
 class Garage():
     '''
     Garage master
     '''
-    def __init__(self, name, lanes):
-        self.name = name
+    def __init__(self, code, lanes):
+        self.code = code
         self.lanes = lanes      #number of lanes in the garage
         self.locked = False    #if locked no further operations allowed
         self.connection = []
@@ -107,71 +117,132 @@ class Garage():
         self.connection.append(lane)
 
 # Unit testing
-if __name__ == '__main__':
-    import copy
-    zone = 'S'
+def create_sample_csv():
+    '''
+    Create sample csv files
+    '''
+    # Stations
     station_list = []
-    oba = StationZone('OBA', 'Obama', zone, 0.0, (-0.140, 0.180))
-    station_list.append(oba)
-    nob = StationZone('NOB', 'Kita Obama', zone, 2.423, (-0.100, 0.180))
-    station_list.append(nob)
-    hns = StationZone('HNS', 'Hinata Shimmachi', zone, 5.679, (-0.100, 0.100))
-    station_list.append(hns)
-    srt = StationZone('SRT', 'Shirato', zone, 8.132, (-0.100, 0.100))
-    station_list.append(srt)
-    hoz = StationZone('HOZ', 'Hozawa', zone, 11.416, (-0.100, 0.100))
-    station_list.append(hoz)
-    tai = StationZone('TAI', 'Takatsu Airport', zone, 14.693, (-0.160, 0.120))
-    station_list.append(tai)
-    mtu = StationZone('MTU', 'Minami Takatsu', zone, 16.910, (-0.090, 0.090))
-    station_list.append(mtu)
-    yan = StationZone('YAN', 'Takatsu Yanaicho', zone, 19.431, (-0.090, 0.090))
-    station_list.append(yan)
-    tku = StationZone('TKU', 'Takatsu', zone, 21.600, (-0.180, 0.160))
-    station_list.append(tku)
-    kta = StationZone('KTA', 'Kita Takatsu', zone, 23.774, (-0.100, 0.100))
-    station_list.append(kta)
-    ire = StationZone('IRE', 'Hase Irie', zone, 25.823, (-0.100, 0.100))
-    station_list.append(ire)
-    hse = StationZone('HSE', 'Hasecho', zone, 28.049, (-0.100, 0.100))
-    station_list.append(hse)
-    hys = StationZone('HYS', 'Hayashidacho', zone, 30.548, (-0.120, 0.140))
-    station_list.append(hys)
-    gom = StationZone('GOM', 'Nakagomicho', zone, 33.200, (-0.100, 0.100))
-    station_list.append(gom)
-    hri = StationZone('HRI', 'Hirai', zone, 35.789, (-0.100, 0.100))
-    station_list.append(hri)
-    print(len(station_list))
+    station_list.append({'code':'OBA', 'name':'Obama'})
+    station_list.append({'code':'NOB', 'name':'Kita Obama'})
+    station_list.append({'code':'HNS', 'name':'Hinata Shimmachi'})
+    station_list.append({'code':'SRT', 'name':'Shirato'})
+    station_list.append({'code':'HOZ', 'name':'Hozawa'})
+    station_list.append({'code':'TAI', 'name':'Takatsu Airport'})
+    station_list.append({'code':'MTU', 'name':'Minami Takatsu'})
+    station_list.append({'code':'YAN', 'name':'Takatsu Yanaicho'})
+    station_list.append({'code':'TKU', 'name':'Takatsu'})
+    station_list.append({'code':'KTA', 'name':'Kita Takatsu'})
+    station_list.append({'code':'IRE', 'name':'Hase Irie'})
+    station_list.append({'code':'HSE', 'name':'Hasecho'})
+    station_list.append({'code':'HYS', 'name':'Hayashidacho'})
+    station_list.append({'code':'GOM', 'name':'Nakagomicho'})
+    station_list.append({'code':'HIR', 'name':'Hirai'})
+    station_list.append({'code':'GTKU', 'name':'Takatsu Garage'})
+    station_list.append({'code':'GOBA', 'name':'Obama Garage'})
 
-    gra = Garage('Takatsu Garage', 20)
+    with open('station_list.csv', 'wt') as csvfile:
+        fieldnames = ['code', 'name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in station_list:
+            writer.writerow(row)
+    print('%s stations are written' % len(station_list))
 
-    lane_list = []
+    # Zone specific station information
+    zone = 'S'
+    station_zone_list = []
+    station_zone_list.append({'code':'OBA', 'zone':zone, 'center_position':0.000, 'station_range':(-0.140, 0.180)})
+    station_zone_list.append({'code':'NOB', 'zone':zone, 'center_position':2.423, 'station_range':(-0.100, 0.180)})
+    station_zone_list.append({'code':'HNS', 'zone':zone, 'center_position':5.679, 'station_range':(-0.100, 0.100)})
+    station_zone_list.append({'code':'SRT', 'zone':zone, 'center_position':8.132, 'station_range':(-0.100, 0.100)})
+    station_zone_list.append({'code':'HOZ', 'zone':zone, 'center_position':11.416, 'station_range':(-0.100, 0.100)})
+    station_zone_list.append({'code':'TAI', 'zone':zone, 'center_position':14.693, 'station_range':(-0.160, 0.120)})
+    station_zone_list.append({'code':'MTU', 'zone':zone, 'center_position':16.910, 'station_range':(-0.090, 0.090)})
+    station_zone_list.append({'code':'YAN', 'zone':zone, 'center_position':19.431, 'station_range':(-0.090, 0.090)})
+    station_zone_list.append({'code':'TKU', 'zone':zone, 'center_position':21.600, 'station_range':(-0.180, 0.160)})
+    station_zone_list.append({'code':'KTA', 'zone':zone, 'center_position':23.774, 'station_range':(-0.100, 0.100)})
+    station_zone_list.append({'code':'IRE', 'zone':zone, 'center_position':25.823, 'station_range':(-0.100, 0.100)})
+    station_zone_list.append({'code':'HSE', 'zone':zone, 'center_position':28.049, 'station_range':(-0.100, 0.100)})
+    station_zone_list.append({'code':'HYS', 'zone':zone, 'center_position':30.548, 'station_range':(-0.120, 0.140)})
+    station_zone_list.append({'code':'GOM', 'zone':zone, 'center_position':33.200, 'station_range':(-0.100, 0.100)})
+    station_zone_list.append({'code':'HIR', 'zone':zone, 'center_position':35.789, 'station_range':(-0.100, 0.100)})
+
+    with open('station_zone.csv', 'wt') as csvfile:
+        fieldnames = ['code', 'zone', 'center_position', 'station_range']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in station_zone_list:
+            writer.writerow(row)
+    print('%s zone specific station information are written' % len(station_zone_list))
+
+    # Tracks
     track_list = []
     prev_station = None
-    for station in station_list:
-        lane1 = Lane(station, 'Lane1', 0.040, 'A', (-0.080, 0.080))
-        lane2 = Lane(station, 'Lane2', -0.040, 'B', (-0.080, 0.080))
-        if station.code == "TKU":   #connect to garage
-            lane1.connection.append(gra)
-            lane2.connection.append(gra)
-        lane_list.append(lane1)
-        lane_list.append(lane2)
+    for station in station_zone_list:
         if prev_station:
-            name = prev_station.code + '-' + station.code
-            track = Track('S', name, 70.0, 'S', (prev_station.center_position, station.center_position))
-            track_list.append(track)
+            if prev_station['code'] + '-' + station['code'] in ('TAI-MTU', 'MTU-YAN', 'YAN-TKU', 'TKU-KTA'):
+                category = 'M'
+            else:
+                category = 'S'
+
+            track_list.append({'name':prev_station['code'] + '-' + station['code'], 'zone':zone,
+                               'speed':70, 'category':category,
+                               'track_range':(prev_station['center_position'], station['center_position'])})
         prev_station = station
-    print(len(lane_list))
 
+    with open('track_list.csv', 'wt') as csvfile:
+        fieldnames = ['name', 'zone', 'speed', 'category', 'track_range']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in track_list:
+            writer.writerow(row)
+    print('%s tracks are written' % len(track_list))
+
+    # Garages
     garage_list = []
-    garage_list.append(gra)
+    garage_list.append({'code':'GOBA', 'lanes':2})
+    garage_list.append({'code':'GTKU', 'lanes':16})
 
-    for track in track_list:
-        if track.name in ('TAI-MTU', 'MTU-YAN', 'YAN-TKU', 'TKU-KTA'):
-            track.type = 'M'
+    with open('garage_list.csv', 'wt') as csvfile:
+        fieldnames = ['code', 'lanes']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in garage_list:
+            writer.writerow(row)
+    print('%s garages are written' % len(garage_list))
 
-    for track in track_list:
-        print(track.name, track.type, track.restriction)
+    # Lanes - assumption : each station has 2 lanes except start and end station (1 lane)
+    lane_list = []
+    for counter, station in enumerate(station_zone_list):
+        connection = []
+        # add tracks for connection
+        if counter > 0:
+            connection.append(track_list[counter-1]['name'])
+        if counter < len(track_list):
+            connection.append(track_list[counter]['name'])
+        # add garages for connection
+        if station['code'] in ('OBA', 'TKU'):
+            connection.append('G' + station['code'])
+        # create lanes
+        if station['code'] not in ('OBA', 'HRI'):
+            lane_list.append({'code':station['code'], 'zone':zone, 'name':'Lane2',
+                              'priority':'B', 'offset':-0.040, 'lane_range':(-0.080, 0.080),
+                              'connection':connection})
+        lane_list.append({'code':station['code'], 'zone':zone, 'name':'Lane1',
+                          'priority':'A', 'offset':0.040, 'lane_range':(-0.080, 0.080),
+                          'connection':connection})
 
-    for lane in lane_list:
-        print(lane.stationzone[0].name, lane.lane, lane.connection)
+    with open('lane_list.csv', 'wt') as csvfile:
+        fieldnames = ['code', 'zone', 'name', 'priority', 'offset', 'lane_range', 'connection']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in lane_list:
+            writer.writerow(row)
+    print('%s lanes are written' % len(lane_list))
+
+if __name__ == '__main__':
+
+    # create csv file anyway
+    create_sample_csv()
+
